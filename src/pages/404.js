2659de19cb7e525de0a2d5478accc08d6cbb183f
Noah Gilmore <noah.w.gilmore@gmail.com>
2019-06-05 10:11:46 -0700

    Slightly better 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,45 @@
 import React from "react"
+import styled from 'styled-components';
+
+const OuterWrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: #125366;
+`;
+
+const Container = styled.div`
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 100px 40px 40px 40px;
+`;
+
+const Heading = styled.h1`
+    color: white;
+    font-size: 42px;
+    font-weight: bold;
+    text-align: center;
+`;
+
+const Subheading = styled.div`
+    color: white;
+    font-size: 16px;
+    line-height: 20px;
+    margin-top: 30px;
+    text-align: center;
+
+    a, a:hover, a:visited {
+        color: inherit;
+        text-decoration: underline;
+    }
+`;
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>Aw man, that's xmark.octagon.fill</p>
-  </div>
+  <OuterWrapper>
+    <Container>
+        <Heading>404: Page not found</Heading>
+        <Subheading>That's a real xmark.octagon.fill</Subheading>
+    </Container>
+  </OuterWrapper>
 )
 
 export default NotFoundPage
